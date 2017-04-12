3516932adb6100c2a606e16bac415d7eeded7234
Jason Herald <jherald@deloitte.com>
2017-04-12 11:56:21 -0400

    Adding the basic echo server

---
+++ mud.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"net"
+)
+
+func requestHandler(conn net.Conn, out chan string) {
+	defer close(out)
+	for {
+		line, err := bufio.NewReader(conn).ReadBytes('\n')
+		if err != nil {
+			return
+		}
+		log.Print(string(line))
+	}
+}
+
+func sendData(conn net.Conn, in <-chan string) {
+	defer conn.Close()
+	for {
+		message := <-in
+		if message != "" {
+			log.Print(message)
+			io.Copy(conn, bytes.NewBufferString(message))
+		}
+	}
+}
+
+func main() {
+	psock, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		return
+	}
+
+	for {
+		conn, err := psock.Accept()
+		if err != nil {
+			return
+		}
+
+		channel := make(chan string)
+		go requestHandler(conn, channel)
+		go sendData(conn, channel)
+	}
+}
