af521f0d5706f378bc0463a1298d2250f90260ee
Noah <noah.wood1@gmail.com>
2017-04-14 14:48:45 +0000

    updates to add combat

--- lib/combat/combat.go
+++ lib/combat/combat.go
@@ -1,32 +1,37 @@
 package combat
 
 import (
+	"fmt"
 	"math/rand"
 )
 
-func Fight() int {
-	pHealth := 100
-	mHealth := 100
+var (
+	pHealth, mHealth, pDam, mDam int64
+)
+
+func Fight() int64 {
+	pHealth = 100
+	mHealth = 100
 	for {
 
-		pDam := rand.Intn(20)
+		pDam = rand.Int63n(20)
 		mHealth = mHealth - pDam
-		//	fmt.Println("Player hits mob for", pDam, ". Mob Health is now", mHealth)
+		fmt.Println("Player hits mob for", pDam, ". Mob Health is now", mHealth)
 		if mHealth <= 0 {
 			break
 		}
 
-		mDam := rand.Intn(20)
+		mDam = rand.Int63n(20)
 		pHealth = pHealth - mDam
-		//	fmt.Println("Mob hits player for", pDam, ". Players Health is now", mHealth)
+		fmt.Println("Mob hits player for", pDam, ". Players Health is now", mHealth)
 		if pHealth <= 0 {
 			break
 		}
-		//	}
-		//	if mHealth <= 0{
-		//	fmt.Println("Mob is dead, your Health is", pHealth)
-		//	} else {
-		//	fmt.Println("Player is dead")
+	}
+	if mHealth <= 0 {
+		fmt.Println("Mob is dead, your Health is", pHealth)
+	} else {
+		fmt.Println("Player is dead")
 	}
 	return (pHealth)
 }
--- lib/connection/handlers.go
+++ lib/connection/handlers.go
@@ -7,20 +7,19 @@ import (
 	"bytes"
 	"io"
 	"net"
+	"strconv"
 	"strings"
 )
 
-//RequestHandler Handles all incoming data
 func RequestHandler(conn net.Conn, out chan string, user objects.User) {
-	defer close(out) // close the connection when we are done
+	defer close(out)
 	for {
-		line, err := bufio.NewReader(conn).ReadBytes('\n') // pull the line from the socket
+		line, err := bufio.NewReader(conn).ReadBytes('\n')
 		if err != nil {
 			return
 		}
 
 		// look up the command which for now is just a movement
-		// this code sucks... lol
 		str := string(line)
 		str = strings.Trim(str, "\n")
 		str = strings.Trim(str, "\r")
@@ -35,22 +34,20 @@ func RequestHandler(conn net.Conn, out chan string, user objects.User) {
 			user.Move(3)
 		case "fight":
 			combats.Fight()
-		case "u":
-			user.Move(4)
-		case "d":
-			user.Move(5)
 		}
-		out <- "Current Position: " + user.ToString() + "\n" // print this mess to the user for now
+		out <- "Current Position: " + user.ToString() + "\n"
+		out <- strconv.FormatInt(combats.Fight(), 10) + "\n"
+
+		//dot  your mom
 	}
 }
 
-//SendData handles all outgoing data
 func SendData(conn net.Conn, in <-chan string) {
-	defer conn.Close() // close when we are done
+	defer conn.Close()
 	for {
-		message := <-in    // get the message we are writing from the channel
-		if message != "" { // if it isn't blank
-			io.Copy(conn, bytes.NewBufferString(message)) // write it to the socket
+		message := <-in
+		if message != "" {
+			io.Copy(conn, bytes.NewBufferString(message))
 		}
 	}
 }
