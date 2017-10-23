58c26855b7b769652b7263375e84188950244d2d
Jason Herald <jason.herald@gmail.com>
2017-10-23 16:22:06 -0400

    Adding the new telnet method

--- mud.go
+++ mud.go
@@ -1,42 +1,49 @@
 package main
 
 import (
-	"net"
-
-	handlers "./lib/connection"
-	objects "./lib/objects"
+	//handlers "./lib/connection"
+	//objects "./lib/objects"
 	sing "./lib/singletons"
 	ticker "./lib/ticker"
 	telnet "github.com/reiver/go-telnet"
+	telsh "github.com/reiver/go-telnet/telsh"
+
+	"io/ioutil"
+	"io"
+	"github.com/reiver/go-oi"
 )
 
 func main() {
 	// new telnet nonsense
-	var handler telnet.Handler = telnet.EchoHandler
-
-	err := telnet.ListenAndServe(":5555", handler)
-	if nil != err {
-		//@TODO: Handle this error better.
-		panic(err)
-	}
-
-	psock, err := net.Listen("tcp", ":5000") // server socket declaration on port 5000
-	if err != nil {                          // if the connection wasn't successful
-		return // stop the program
-	}
+	shellHandler := telsh.NewShellHandler()
 
 	// load in and parse the xml messages
 	sing.GetMessagesInstance()
+
+	content, err := ioutil.ReadFile("MOTD") // load the file contents for the MOTD
+	if err != nil {                         // if reading the file wasn't successful
+		panic(1) // oh dear god!!!!
+	}
+
+	shellHandler.WelcomeMessage = string(content)
+
+	commandName     := "five"
+	commandProducer := telsh.ProducerFunc(fiveProducer)
+
+	shellHandler.Register(commandName, commandProducer)
 
 	// spin up the ticker
 	ticker.Start()
 	defer ticker.Stop()
 
+	addr := ":5000"
+	if err := telnet.ListenAndServe(addr, shellHandler); nil != err {
+		panic(err)
+	}
+
+	/*
 	for { // loop forever
-		conn, err := psock.Accept() // accept the incoming connection
-		if err != nil {             // if the connection wasn't successful
-			return // stop the program
-		}
+
 
 		channel := make(chan string) // build a channel for concurrency
 
@@ -54,4 +61,16 @@ func main() {
 		go handlers.SendData(conn, channel)             // send data
 		sing.GetUsersInstance().Add(user)               // register this user in the singleton
 	}
+	*/
+}
+
+
+func fiveHandler(stdin io.ReadCloser, stdout io.WriteCloser, stderr io.WriteCloser, args ...string) error {
+	oi.LongWriteString(stdout, "The number FIVE looks like this: 5\r\n")
+
+	return nil
+}
+
+func fiveProducer(ctx telnet.Context, name string, args ...string) telsh.Handler {
+	return telsh.PromoteHandlerFunc(fiveHandler)
 }
